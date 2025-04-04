741c5066fbc813e3d7e080d7fde58001805f9897
Vihaan Shah <58313595+vihaanshah2014@users.noreply.github.com>
2025-04-04 19:30:45 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "JellyJelly - Open Source Video Chat",
+  title: "jellyjelly.dev",
   description: "Join us in revolutionizing social media through open-source collaboration. Together, we're building the future of video chat.",
 };
 
